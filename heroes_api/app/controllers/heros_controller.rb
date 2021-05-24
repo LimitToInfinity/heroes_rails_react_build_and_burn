aa4929f53bf6d0dfc33bb7b6d2440a7004181340
LimitToInfinity <21stLimitless@gmail.com>
2021-05-24 14:36:21 -0600

    Uses strong params for hero creation.

--- heroes_api/app/controllers/heros_controller.rb
+++ heroes_api/app/controllers/heros_controller.rb
@@ -6,16 +6,14 @@ class HerosController < ApplicationController
   end
 
   def create
-    @hero = Hero.create(
-      name: params[:name],
-      weapon_id: params[:weapon_id]
-    )
-
-    params[:powers].each do |power_id|
-      HeroPower.create hero: @hero, power_id: power_id
-    end
-
+    @hero = Hero.create hero_params
     render json: @hero, include: [:weapon, :powers]
+  end
+
+  private
+
+  def hero_params
+    params.require(:hero).permit(:name, :weapon_id, power_ids: [])
   end
 
 end
